80bc798fc937ece8d880028c62a3281d78063e5c
DRoMarin <159dram@gmail.com>
2023-04-04 13:46:37 -0600

    agrega label para display y botones que simulan sensores, falta comando

--- panel.py
+++ panel.py
@@ -1,13 +1,21 @@
 import tkinter as tk
 from queue import Queue
 
+panel = tk.Tk()
+panel_screen_buffer = tk.StringVar()
+panel_screen_buffer.set("--------")
 panel_buffer = Queue(maxsize = 8)
-panel_screen_buffer = []
+
+sensor_num = 16
 
 button_labels = ["1","2","3","Esc"  ,
                  "4","5","6","Enter",
                  "7","8","9","PANIC",
                  "*","0","#","INCEN"]
+
+def updateScreen(input_buffer):
+    buffer = list(input_buffer.queue)
+    panel_screen_buffer.set(''.join(buffer))
 
 def buttonCallback(button_pressed):
     label = button_labels[button_pressed]
@@ -19,21 +27,22 @@ def buttonCallback(button_pressed):
                 panel_buffer.put(label)
             else:
                 panel_buffer.put(label)
+            updateScreen(panel_buffer)
         case "Esc" :
             panel_buffer.queue.clear()
+            updateScreen(panel_buffer)
         case "Enter":
-            print(*list(panel_buffer.queue),sep=" ")
+            print(*list(panel_buffer.queue),sep="")
             panel_buffer.queue.clear()
+            updateScreen(panel_buffer)
         case _:
             print("TBD")
 
-
 ##### configuracion ventana principal
-panel = tk.Tk()
-#panel.title('PANEL SIMULADOR')label
+panel.title('PANEL SIMULADOR')
 #tamaño
-panel_w = 1000
-panel_h = 200
+panel_w = 1100
+panel_h = 250
 #centrado
 screen_w = panel.winfo_screenwidth()
 screen_h = panel.winfo_screenheight()
@@ -43,7 +52,8 @@ center_x = int(screen_w/2)# - panel_w/2)
 #panel.geometry(f'{panel_w}x{panel_h}+{center_x}+{center_y}')
 panel.resizable(False,False)
 
-
+panel_screen = tk.Label(panel,textvariable=panel_screen_buffer,font=('Hack',32),height=2)
+panel_screen.grid(row=0,column=0,columnspan=4,sticky="ew")
 
 #array de botones
 pixel = tk.PhotoImage(width = 1,height = 1)
@@ -51,15 +61,32 @@ B_arr = [0,0]*len(button_labels)
 for label_idx in range(len(button_labels)):
     B_arr[label_idx] = tk.Button(panel,
                                  text=button_labels[label_idx],
-                                 width = 200, height = 50,
+                                 width = 60, height = 60,
                                  padx=0,pady=0,
                                  image=pixel, 
                                  compound='center',
                                  highlightthickness=0,
                                  command=lambda i = label_idx: buttonCallback(i))
-    B_arr[label_idx].grid(row = (label_idx//4), column = (label_idx % 4))
+    B_arr[label_idx].grid(row = (label_idx//4)+1, column = (label_idx % 4),sticky="ew")
 
-panel.grid_columnconfigure(1,weight=0)
+spacing = tk.Label(panel,text="",font=('Consolas',40),width=2)
+spacing.grid(row=1, column=4)
+
+# Array de sensores
+S_arr = [0,0]*sensor_num
+for sensor_idx in range(sensor_num):
+    S_arr[sensor_idx] = tk.Button(panel,
+                                 text=str(sensor_idx),
+                                 width = 60, height = 60,
+                                 padx=0,pady=0,
+                                 image=pixel, 
+                                 compound='center',
+                                 highlightthickness=0,
+                                 #command=lambda i = label_idx: buttonCallback(i)
+                                 )
+    S_arr[sensor_idx].grid(column = (sensor_idx//4)+5, row = (sensor_idx % 4)+1)
+
+
 
 #B = tk.Button(panel, text = "APRETAR", command = buttonCallback)
 #B.pack()
