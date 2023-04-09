2b68da0af24a05a9e90dcea14d9566e12e04364b
DRoMarin <159dram@gmail.com>
2023-04-08 21:57:54 -0600

    agrega panel completo con indicadores y sensores.

--- panel.py
+++ panel.py
@@ -7,10 +7,13 @@ panel_screen_buffer = tk.StringVar()
 panel_buffer = Queue(maxsize = 8)
 sensor_num = 16
 
+
 button_labels = ["1","2","3","Esc"  ,
                  "4","5","6","Enter",
                  "7","8","9","PANIC",
                  "*","0","#","INCEN"]
+
+led_labels = ["MODO 0","MODO 1","BATT"]
 
 def updateScreen(input_buffer):
     buffer = list(input_buffer.queue)
@@ -37,18 +40,14 @@ def buttonCallback(button_pressed):
         case _:
             print("TBD") #################################################
 
+
+
+def sensorCallback(sensor_pressed):
+    print(sensor_pressed)
+
 ##### configuracion ventana principal
 panel.title('PANEL SIMULADOR')
 #tamaño
-#panel_w = 650
-#panel_h = 500
-#centrado
-screen_w = panel.winfo_screenwidth()
-screen_h = panel.winfo_screenheight()
-#center_y = int(screen_h/2 - panel_h/2) 
-#center_x = int(screen_w/2 - panel_w/2)
-
-#panel.geometry(f'{panel_w}x{panel_h}+{center_x}+{center_y}')
 panel.resizable(False,False)
 
 #panel screen
@@ -61,15 +60,31 @@ panel_screen = tk.Label(panel_screen_frame,
 panel_screen.pack(side="top")
 
 led_screen_frame = tk.LabelFrame(panel_screen_frame,
-                                 height=500,
+                                 height=30,
                                  width=1000,
                                  borderwidth=1,
                                  highlightthickness=0)
 led_screen_frame.pack(side="bottom")
 
+#Indicadores LED
+led_arr = [[0]*2]*len(led_labels)
+for ind_idx in range(len(led_labels)):
+    led_arr[ind_idx][0] = tk.Label(led_screen_frame,
+                                   text= led_labels[ind_idx],
+                                   font = ('Consolas',10))
+    
+    led_arr[ind_idx][1] = tk.Canvas(led_screen_frame,
+                                    width=20,
+                                    height=20,
+                                    bg='gray')
+    
+    led_arr[ind_idx][0].grid(row = 0, column=ind_idx*2)
+    led_arr[ind_idx][1].grid(row = 0, column= ind_idx*2+1)
+
+
 #array de botones
 pixel = tk.PhotoImage(width = 1,height = 1)
-B_arr = [0,0]*len(button_labels)
+B_arr = [0]*len(button_labels)
 for label_idx in range(len(button_labels)):
     B_arr[label_idx] = tk.Button(panel,
                                  text=button_labels[label_idx],
@@ -86,7 +101,7 @@ spacing = tk.Label(panel,text="",font=('Consolas',40),width=2)
 spacing.grid(row=1, column=4)
 
 # Array de sensores
-S_arr = [0,0]*sensor_num
+S_arr = [0]*sensor_num
 for sensor_idx in range(sensor_num):
     S_arr[sensor_idx] = tk.Button(panel,
                                  text=str(sensor_idx),
@@ -96,8 +111,7 @@ for sensor_idx in range(sensor_num):
                                  compound='center',
                                  highlightthickness=5,
                                  borderwidth=3,
-                                 #command=lambda i = label_idx: buttonCallback(i)
-                                 )
+                                 command=lambda i = sensor_idx: sensorCallback(i))
     S_arr[sensor_idx].grid(column = (sensor_idx//4)+5, row = (sensor_idx % 4)+1)
 
 
