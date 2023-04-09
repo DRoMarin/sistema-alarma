cf21c97118b868a302ea1d039a3aa429e8970caa
DRoMarin <159dram@gmail.com>
2023-04-09 01:15:57 -0600

    agrega segundo hilo y colas/eventos. Falta FSM

--- panel.py
+++ panel.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from queue import Queue
+import time
+import threading as th
+
+#import sistema
 
 panel = tk.Tk()
 panel_screen_buffer = tk.StringVar()
@@ -21,6 +25,7 @@ def updateScreen(input_buffer):
 
 def buttonCallback(button_pressed):
     label = button_labels[button_pressed]
+    #TBD: AGREGAR FUNCIONES SISTEMA
     match label:
         case index if label in ["1","2","3","4","5","6","7","8","9","*","0","#"]:
             print(label)
@@ -34,16 +39,29 @@ def buttonCallback(button_pressed):
             panel_buffer.queue.clear()
             updateScreen(panel_buffer)
         case "Enter":
-            print(*list(panel_buffer.queue),sep="")
+            command = ''.join(panel_buffer.queue)
+            print(command)
+            panel_queue.put(command)
+            panel_event.set()
             panel_buffer.queue.clear()
             updateScreen(panel_buffer)
         case _:
             print("TBD") #################################################
 
-
-
 def sensorCallback(sensor_pressed):
+    #TBD: AGREGAR FUNCIONES SISTEMA
     print(sensor_pressed)
+
+def systemTask():
+    while True:
+        if close_event.is_set():
+            break
+        if panel_event.is_set():
+            command = panel_queue.get()
+            print(command)
+            panel_event.clear()
+    print("CERRANDO")
+    
 
 ##### configuracion ventana principal
 panel.title('PANEL SIMULADOR')
@@ -51,19 +69,19 @@ panel.title('PANEL SIMULADOR')
 panel.resizable(False,False)
 
 #panel screen
-panel_screen_frame = tk.LabelFrame(panel, height=130,borderwidth=3,highlightthickness=5)
-panel_screen_frame.grid(row=0,column=0,columnspan=4,sticky='nsew')
+panel_screen_frame = tk.LabelFrame(panel, height = 130,borderwidth = 3,highlightthickness = 5)
+panel_screen_frame.grid(row = 0,column = 0,columnspan = 4,sticky = 'nsew')
 panel_screen_frame.pack_propagate(0)
 panel_screen = tk.Label(panel_screen_frame,
-                        textvariable=panel_screen_buffer,
-                        font=('Consolas',40))
-panel_screen.pack(side="top")
+                        textvariable = panel_screen_buffer,
+                        font = ('Consolas',40))
+panel_screen.pack(side = "top")
 
 led_screen_frame = tk.LabelFrame(panel_screen_frame,
-                                 height=30,
-                                 width=1000,
-                                 borderwidth=1,
-                                 highlightthickness=0)
+                                 height = 30,
+                                 width = 1000,
+                                 borderwidth = 1,
+                                 highlightthickness = 0)
 led_screen_frame.pack(side="bottom")
 
 #Indicadores LED
@@ -74,12 +92,12 @@ for ind_idx in range(len(led_labels)):
                                    font = ('Consolas',10))
     
     led_arr[ind_idx][1] = tk.Canvas(led_screen_frame,
-                                    width=20,
-                                    height=20,
-                                    bg='gray')
+                                    width = 20,
+                                    height = 20,
+                                    bg = 'gray')
     
-    led_arr[ind_idx][0].grid(row = 0, column=ind_idx*2)
-    led_arr[ind_idx][1].grid(row = 0, column= ind_idx*2+1)
+    led_arr[ind_idx][0].grid(row = 0, column = ind_idx*2)
+    led_arr[ind_idx][1].grid(row = 0, column = ind_idx*2+1)
 
 
 #array de botones
@@ -87,14 +105,14 @@ pixel = tk.PhotoImage(width = 1,height = 1)
 B_arr = [0]*len(button_labels)
 for label_idx in range(len(button_labels)):
     B_arr[label_idx] = tk.Button(panel,
-                                 text=button_labels[label_idx],
+                                 text = button_labels[label_idx],
                                  width = 50, height = 50,
-                                 padx=0,pady=0,
-                                 image=pixel, 
-                                 compound='center',
-                                 highlightthickness=5,
-                                 borderwidth=3,
-                                 command=lambda i = label_idx: buttonCallback(i))
+                                 padx = 0,pady = 0,
+                                 image = pixel, 
+                                 compound = 'center',
+                                 highlightthickness = 5,
+                                 borderwidth = 3,
+                                 command = lambda i = label_idx: buttonCallback(i))
     B_arr[label_idx].grid(row = (label_idx//4)+1, column = (label_idx % 4),sticky="ew")
 
 spacing = tk.Label(panel,text="",font=('Consolas',40),width=2)
@@ -104,17 +122,25 @@ spacing.grid(row=1, column=4)
 S_arr = [0]*sensor_num
 for sensor_idx in range(sensor_num):
     S_arr[sensor_idx] = tk.Button(panel,
-                                 text=str(sensor_idx),
+                                 text = str(sensor_idx),
                                  width = 50, height = 50,
-                                 padx=0,pady=0,
-                                 image=pixel, 
-                                 compound='center',
-                                 highlightthickness=5,
-                                 borderwidth=3,
-                                 command=lambda i = sensor_idx: sensorCallback(i))
+                                 padx = 0,pady = 0,
+                                 image = pixel, 
+                                 compound ='center',
+                                 highlightthickness = 5,
+                                 borderwidth = 3,
+                                 command = lambda i = sensor_idx: sensorCallback(i))
     S_arr[sensor_idx].grid(column = (sensor_idx//4)+5, row = (sensor_idx % 4)+1)
 
+system_thread = th.Thread(target=systemTask)
 
+#eventos
+panel_event = th.Event()
+close_event = th.Event()
+#colas
+panel_queue = Queue()
 
+system_thread.start()
 panel.mainloop()
-
+close_event.set()
+system_thread.join()
